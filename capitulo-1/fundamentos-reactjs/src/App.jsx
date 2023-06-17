99d7ef4a8db1c5d02f4bfaab9cb1479be6270385
Eduardo Tonatto <edtonatto@gmail.com>
2023-06-17 20:10:52 -0300

    :construction: Iniciado estilização com CSS Modules

--- capitulo-1/fundamentos-reactjs/src/App.jsx
+++ capitulo-1/fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from './components/Header.jsx'
 import { Post } from './Post.jsx'
+
+import './global.css'
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post author="Eduardo Tonatto" content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui sint, deserunt unde optio, nostrum dignissimos odit tenetur, voluptatem expedita nam esse error blanditiis dolore? Labore temporibus explicabo autem deleniti cumque?"/>
       <Post author="Artur Tonatto" content = "Oi, meu nome eh Artur, tudo bem com vc?"/>
     </div>
