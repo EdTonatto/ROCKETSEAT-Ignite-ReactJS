ad5986fba25022fec21bf4c2c0c77f9352529cf3
Eduardo Tonatto <edtonatto@gmail.com>
2023-06-17 19:38:25 -0300

    :sparkles: Implementado conceitos de Componentes/Propriedades

--- capitulo-1/fundamentos-reactjs/src/App.jsx
+++ capitulo-1/fundamentos-reactjs/src/App.jsx
@@ -1,7 +1,10 @@
-function App() {
+import { Post } from './Post.jsx'
+
+export function App() {
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <Post author="Eduardo Tonatto" content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui sint, deserunt unde optio, nostrum dignissimos odit tenetur, voluptatem expedita nam esse error blanditiis dolore? Labore temporibus explicabo autem deleniti cumque?"/>
+      <Post author="Artur Tonatto" content = "Oi, meu nome eh Artur, tudo bem com vc?"/>
+    </div>
   )
 }
-
-export default App
