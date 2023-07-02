a08186950b474ab64da2c9357d292975c0a10e2e
Eduardo Tonatto <edtonatto@gmail.com>
2023-07-02 18:25:42 -0300

    :sparkles: Criação de post de acordo com objeto de posts

--- capitulo-1/fundamentos-reactjs/src/App.jsx
+++ capitulo-1/fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,27 @@
+/* eslint-disable react/jsx-key */
 import { Header } from './components/Header.jsx'
 import { Post } from './components/Post.jsx'
 
 import './global.css'
 import styles from './App.module.css'
 import { Sidebar } from './components/Sidebar.jsx'
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/EdTonatto.png',
+      name: 'Eduardo Tonatto',
+      role: 'Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu github. É um projeto que fiz enquanto estudava React na Rocketseat. O nome do projeto é Game Notes 🚀' },
+      { type: 'link', content: 'edtonatto.github.io/game-notes' },
+    ],
+    publishedAt: new Date('2023-07-01 18:00:00'),
+  }
+];
 
 export function App() {
   return (
@@ -13,8 +31,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="Eduardo Tonatto" content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui sint, deserunt unde optio, nostrum dignissimos odit tenetur, voluptatem expedita nam esse error blanditiis dolore? Labore temporibus explicabo autem deleniti cumque?"/>
-          <Post author="Artur Tonatto" content = "Oi, meu nome eh Artur, tudo bem com vc?"/>
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>  
     </div>
--- capitulo-1/fundamentos-reactjs/src/components/Post.jsx
+++ capitulo-1/fundamentos-reactjs/src/components/Post.jsx
@@ -1,30 +1,46 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/EdTonatto.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Eduardo Tonatto</strong>
-            <span>Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title='17 de Junho as 20:10' dateTime='2023-06-17 20:10:00'>Publicado ha 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href="">jane.design/doctorcare</a></p>
-        <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a></p>
+        {content.map(item => {
+          if(item.type === 'paragraph') {
+            return <p>{item.content}</p>;
+          } else if (item.type === 'link') {
+            return <p><a href='#'>{item.content}</a></p>
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
